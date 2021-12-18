6d504388cea76c5dcb419f03ae3f96ba84e54f44
Max <kolxy@qq.com>
2021-12-18 11:49:30 -0800

    list category

---
+++ src/LeetCode/list/Lc0021MergeTwoSortedLists.java
@@ -0,0 +1,24 @@
+package LeetCode.list;
+
+import LeetCode.LcTemplate;
+
+public class Lc0021MergeTwoSortedLists implements LcTemplate {
+
+    @Override
+    public void run() {
+
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        return null;
+    }
+
+}
