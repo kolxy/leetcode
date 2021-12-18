697169e2292cf020e66d3cf96ec88454002956d8
Max <kolxy@qq.com>
2021-12-18 12:55:17 -0800

    0021

--- src/LeetCode/list/Lc0021MergeTwoSortedLists.java
+++ src/LeetCode/list/Lc0021MergeTwoSortedLists.java
@@ -18,7 +18,26 @@ public class Lc0021MergeTwoSortedLists implements LcTemplate {
     }
 
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        return null;
+        ListNode head = new ListNode(), pointer = head;
+        ListNode pointer1 = list1, pointer2 = list2;
+        while (pointer1 != null || pointer2 != null) {
+            int val1 = Integer.MAX_VALUE, val2 = Integer.MAX_VALUE;
+            if (pointer1 != null) {
+                val1 = pointer1.val;
+            }
+            if (pointer2 != null) {
+                val2 = pointer2.val;
+            }
+            ListNode temp = new ListNode(Math.min(val1, val2));
+            pointer.next = temp;
+            pointer = pointer.next;
+            if (val1 < val2) {
+                pointer1 = pointer1.next;
+            } else {
+                pointer2 = pointer2.next;
+            }
+        }
+        return head.next;
     }
 
 }
