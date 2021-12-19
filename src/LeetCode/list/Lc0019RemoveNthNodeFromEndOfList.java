319b35ae5fea708a5896da5933ff4b937bce09e6
Max <kolxy@qq.com>
2021-12-18 22:19:00 -0800

    19 Remove N-th Node from End of List

--- src/LeetCode/list/Lc0019RemoveNthNodeFromEndOfList.java
+++ src/LeetCode/list/Lc0019RemoveNthNodeFromEndOfList.java
@@ -8,5 +8,27 @@ public class Lc0019RemoveNthNodeFromEndOfList implements LcTemplate {
 
     }
 
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
 
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode p1 = dummy, p2 = dummy;
+        while (n > 0 && p2.next != null) {
+            p2 = p2.next;
+            n--;
+        }
+
+        while (p2.next != null) {
+            p2 = p2.next;
+            p1 = p1.next;
+        }
+        p1.next = p1.next.next;
+        return dummy.next;
+    }
 }
