0fba48af0145a3b619bc5de81f8d92a3c823b890
Max <kolxy@qq.com>
2021-12-21 13:23:01 -0800

    102 binary tree order traversal

---
+++ src/LeetCode/binarytree/Lc0102BinaryTreeOrderTraversal.java
@@ -0,0 +1,59 @@
+package LeetCode.binarytree;
+
+import LeetCode.LcTemplate;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Lc0102BinaryTreeOrderTraversal implements LcTemplate {
+    @Override
+    public void run() {
+
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    // Basically BFS
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> list = new ArrayList<>();
+        if (root == null) {
+            return list;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                level.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            if (level.size() != 0) {
+                list.add(level);
+            }
+        }
+
+        return list;
+    }
+}
