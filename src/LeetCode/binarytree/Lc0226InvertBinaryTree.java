7504e9085430d0adda01e105044ec4f0d91c67ae
Max <kolxy@qq.com>
2021-12-21 14:51:18 -0800

    226 invert binary tree

---
+++ src/LeetCode/binarytree/Lc0226InvertBinaryTree.java
@@ -0,0 +1,35 @@
+package LeetCode.binarytree;
+
+import LeetCode.LcTemplate;
+
+public class Lc0226InvertBinaryTree implements LcTemplate {
+    @Override
+    public void run() {
+
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
