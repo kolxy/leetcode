b6d2b808d2013b3c46eb4ade57ff52a8e0ba92d0
Max <kolxy@qq.com>
2021-12-23 14:44:02 -0800

    230 kth smallest element in a bst

---
+++ src/LeetCode/bst/Lc0230KthSmallestElementInBST.java
@@ -0,0 +1,42 @@
+package LeetCode.bst;
+
+import LeetCode.LcTemplate;
+import LeetCode.binarytree.Lc0652FindDuplicateSubtrees;
+
+public class Lc0230KthSmallestElementInBST implements LcTemplate {
+    @Override
+    public void run() {
+
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    private int val;
+    public int kthSmallest(TreeNode root, int k) {
+        traverse(root, k, 0);
+        return val;
+    }
+
+    public int traverse(TreeNode root, int k, int count) {
+        if (root == null) {
+            return count;
+        }
+        int current = traverse(root.left, k, count) + 1;
+        if (current == k) {
+            val = root.val;
+        }
+        current = traverse(root.right, k, current);
+        return current;
+    }
+}
