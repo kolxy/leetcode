d45353f45a511d852cf957ce7f22de1c65833ef6
Max <kolxy@qq.com>
2021-12-24 01:17:02 -0800

    701 Insert into a binary search tree

---
+++ src/LeetCode/bst/Lc0701InsertIntoBinarySearchTree.java
@@ -0,0 +1,35 @@
+package LeetCode.bst;
+
+import LeetCode.LcTemplate;
+
+public class Lc0701InsertIntoBinarySearchTree implements LcTemplate {
+    @Override
+    public void run() {
+
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+        if (val < root.val) {
+            root.left = insertIntoBST(root.left, val);
+        } else if (val > root.val) {
+            root.right = insertIntoBST(root.right, val);
+        }
+        return root;
+    }
+}
