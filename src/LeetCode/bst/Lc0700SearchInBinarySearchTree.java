715f333d3026773536c3d07db7387d2301a5954b
Max <kolxy@qq.com>
2021-12-24 01:18:25 -0800

    700 Search in a binary search tree

---
+++ src/LeetCode/bst/Lc0700SearchInBinarySearchTree.java
@@ -0,0 +1,35 @@
+package LeetCode.bst;
+
+import LeetCode.LcTemplate;
+
+public class Lc0700SearchInBinarySearchTree implements LcTemplate {
+    @Override
+    public void run() {
+
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+        if (val < root.val) {
+            return searchBST(root.left, val);
+        } else if (val > root.val) {
+            return searchBST(root.right, val);
+        }
+        return root;
+    }
+}
