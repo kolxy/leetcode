8f0879113081f5084bc84a2f786414d1c61c9aaa
Max <kolxy@qq.com>
2021-12-27 23:49:38 -0800

    72. Edit Distance

---
+++ src/LeetCode/dp/Lc0072EditDistance.java
@@ -0,0 +1,50 @@
+package LeetCode.dp;
+
+import LeetCode.LcTemplate;
+
+import java.util.Arrays;
+
+public class Lc0072EditDistance implements LcTemplate {
+    @Override
+    public void run() {
+
+    }
+
+    int[][] memo;
+    String w1;
+    String w2;
+    public int minDistance(String word1, String word2) {
+        memo = new int[word1.length() + 1][word2.length() + 1];
+        w1 = word1;
+        w2 = word2;
+        for (int i = 0; i < memo.length; i++) {
+            Arrays.fill(memo[i], Integer.MAX_VALUE);
+        }
+        memo[0][0] = 0;
+        return dp(word1.length(), word2.length());
+    }
+
+    public int dp(int i, int j) {
+        if (memo[i][j] != Integer.MAX_VALUE) {
+            return memo[i][j];
+        }
+        if (i == 0) {
+            memo[i][j] = j;
+            return j;
+        }
+        if (j == 0) {
+            memo[i][j] = i;
+            return i;
+        }
+        int min = min(dp(i-1, j-1), dp(i-1, j), dp(i, j-1)) + 1;
+        if (w1.charAt(i-1) == w2.charAt(j-1)) {
+            min = memo[i-1][j-1];
+        }
+        memo[i][j] = min;
+        return min;
+    }
+
+    public int min(int a, int b, int c) {
+        return Math.min(Math.min(a, b), c);
+    }
+}
